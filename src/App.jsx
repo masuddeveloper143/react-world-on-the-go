9602c5f704c7939bdee45f12fb5a7e73e43657c5
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-31 22:00:20 +0800

    Update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 
+import { Suspense } from 'react';
 import './App.css'
+import Countries from './components/countries'
+
+const countriesPromise = fetch("https://restcountries.com/v3.1/all?fields=name,capital,currencies")
+  .then(res => res.json());
 
 function App() {
 
@@ -7,6 +12,9 @@ function App() {
 
     <>
       <h1>react + vite</h1>
+      <Suspense fallback={<h1> nadir on the go....</h1>}>
+        <Countries countriesPromise={countriesPromise} ></Countries>
+      </Suspense>
 
     </>
   )
--- src/components/countries.jsx
+++ src/components/countries.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const countries = () => {
+const countries = ({ countriesPromise }) => {
     return (
         <div>
             <h1>trabeling countries</h1>
