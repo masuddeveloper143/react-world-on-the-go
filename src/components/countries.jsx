2a9ac7f32536ea3c528a4263e0082ae59214ad74
Masud Rana <md.masudrana.developer@gmail.com>
2026-04-01 11:31:55 +0800

    Update project

--- src/components/countries.jsx
+++ src/components/countries.jsx
@@ -1,15 +1,22 @@
 import React, { use } from 'react';
 import Country from './countries/country/country';
-import "./countrie.css";
+
+import './countries/countries.css'
+
+
+
 const countries = ({ countriesPromise }) => {
     const countries = use(countriesPromise);
     console.log(countries);
     return (
-        <div className='countries'>
+        <div>
             <h1>trabeling countries: {countries.length}</h1>
-            {
-                countries.map(country => <Country country={country} ></Country>)
-            }
+
+            <div className='countries'>
+                {
+                    countries.map(country => <Country country={country} ></Country>)
+                }
+            </div>
         </div>
     );
 };
